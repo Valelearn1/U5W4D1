5d63e4e238eee74486b690193cb3630d573af640
Valentina Ferro <valentina-ferro@hotmail.it>
2026-09-07 13:58:38 +0000

    Primo commit: integrazione frontend e backend

---
+++ BE/src/main/java/valentinaferro/u5w4d1/U5W4D1Application.java
@@ -0,0 +1,13 @@
+package valentinaferro.u5w4d1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W4D1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W4D1Application.class, args);
+    }
+
+}
---
+++ BE/src/test/java/valentinaferro/u5w4d1/U5W4D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package valentinaferro.u5w4d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W4D1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
